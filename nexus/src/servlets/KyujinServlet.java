c8ac16d79f2eecb81c402873fa22d3c17856acd3
HayatoTomonari <tomonari1807adash@outlook.jp>
2018-09-14 16:15:43 +0900

    KyujinServlet編集

--- nexus/src/servlets/KyujinServlet.java
+++ nexus/src/servlets/KyujinServlet.java
@@ -15,7 +15,7 @@ import jp.or.adash.nexus.services.KyujinService;
 /**
  * Servlet implementation class KyujinServlet
  */
-@WebServlet("/jobsearch")
+@WebServlet("/web/jobsearch")
 public class KyujinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
