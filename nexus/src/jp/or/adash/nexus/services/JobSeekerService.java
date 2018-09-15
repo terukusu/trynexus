ab20e4ed168455a2016769167d1e139e48353933
czzh8f6aztgsr <okamura1807adash@outlook.jp>
2018-09-15 13:49:23 +0900

    求職者新規登録

--- nexus/src/jp/or/adash/nexus/services/JobSeekerService.java
+++ nexus/src/jp/or/adash/nexus/services/JobSeekerService.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jp.or.adash.nexus.entity.JobSeeker;
+import jp.or.adash.nexus.entity.Jobseeker_simple_entity;
+import jp.or.adash.nexus.utils.common.MessageCommons;
 import jp.or.adash.nexus.utils.dao.JobSeekerDao;
+import jp.or.adash.nexus.utils.dao.JobSeeker_dao;
 import jp.or.adash.nexus.utils.dao.Transaction;
 
 
@@ -29,7 +32,56 @@ public class JobSeekerService {
 		transaction = new Transaction();
 		messages = new ArrayList<String>();
 	}
+	/**
+	 * 求職者idを元に絞込み、求職者情報を取得する
+	 * @param id 求職者id
+	 * @return idで絞り込んだ求職者情報
+	 */
+	public Jobseeker_simple_entity getJobseeker(int js_id) {
+		Jobseeker_simple_entity jobseeker = null;
 
+		try {
+			// データベース接続を開始する
+			transaction.open();
+
+			// 商品単価を取得する
+			JobSeeker_dao dao = new JobSeeker_dao(transaction);
+			jobseeker = dao.selectItem(js_id);
+
+		} catch(IOException e) {
+			// エラーメッセージをセットする
+		} finally {
+			// データベース接続をを終了する
+			transaction.close();
+		}
+
+		return jobseeker;
+	}
+
+	/**
+	 * 求職者情報一覧を取得する
+	 * @return 商品リスト
+	 */
+public List<Jobseeker_simple_entity> getJobSeekerList(){
+	List<Jobseeker_simple_entity> jobseekerList = new ArrayList<Jobseeker_simple_entity>();
+	try {
+		// データベース接続を開始する
+		transaction.open();
+
+		// 商品リストを取得する
+		JobSeeker_dao dao = new JobSeeker_dao(transaction);
+		jobseekerList = dao.selectJobseekerList();
+
+	} catch(IOException e) {
+		// エラーメッセージをセットする
+		messages.add(MessageCommons.ERR_DB_CONNECT);
+	} finally {
+		// データベース接続を終了する
+		transaction.close();
+	}
+
+	return jobseekerList;
+	}
 
 	/**
 	 * 求職者IDを元に、求職者情報を取得する
