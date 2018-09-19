7b94b034ac1d4529577f17d88e9fb9d63260857e
HayatoTomonari <tomonari1807adash@outlook.jp>
2018-09-19 14:03:57 +0900

    AccountList編集。

--- nexus/src/jp/or/adash/nexus/utils/dao/AccountListDao.java
+++ nexus/src/jp/or/adash/nexus/utils/dao/AccountListDao.java
@@ -1,5 +1,64 @@
 package jp.or.adash.nexus.utils.dao;
 
-public class AccountListDao {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import jp.or.adash.nexus.entity.Staff;
+
+public class AccountListDao {
+	/**
+	 * データベース接続オブジェクト
+	 */
+	private Connection conn;
+
+	/**
+	 * コンストラクタ
+	 * @param transaction トランザクションオブジェクト
+	 */
+	public AccountListDao(Transaction transaction) {
+		this.conn = transaction.getConnection();
+	}
+	/**
+	 * アカウントの情報一覧を取得する
+	 * @return アカウントリスト
+	 * @throws IOException
+	 */
+	public List<Staff> selectAccountList() throws IOException {
+		List<Staff> account = new ArrayList<Staff>();
+
+		// SQL文を生成する
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT id,name,kana,authority");
+		sql.append(" from staff");
+		sql.append(" order by id asc");
+		try (PreparedStatement ps = this.conn.prepareStatement(sql.toString())) {
+			// SQL文を実行する
+			try (ResultSet rs = ps.executeQuery()) {
+				//0取得結果をリストに格納する
+				while(rs.next()) {
+					account.add(new Staff
+							(rs.getString("id"),
+							rs.getString("name"),
+							rs.getString("kana"),
+							rs.getString("authority"),
+							null,
+							null,
+							null,
+							null,
+							null,
+							null));
+				}
+			} catch(SQLException e) {
+				throw new IOException(e);
+			}
+		} catch(SQLException e) {
+			throw new IOException(e);
+		}
+		return account;
+	}
 }
