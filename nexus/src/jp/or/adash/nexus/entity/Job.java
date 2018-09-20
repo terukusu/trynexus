143d3d900592a7973e340ca20ec7ee6e9c3b5f4c
AWU316 <AWU316@AW316.adash.local>
2018-09-20 15:19:41 +0900

    職種マスタ・業種マスタにidを追加。採番に職業紹介者採番を追加。

--- nexus/src/jp/or/adash/nexus/entity/Job.java
+++ nexus/src/jp/or/adash/nexus/entity/Job.java
@@ -8,21 +8,32 @@ package jp.or.adash.nexus.entity;
 public class Job {
 
 	/**
+	 * id 職種ID
 	 * largecd 大分類コード
 	* middlecd 中分類コード
 	* smallcd 小分類コード
 	* name 業種名
 	 */
+	private int id;
 	private String largecd;
 	private String middlecd;
 	private String smallcd;
 	private String name;
 
-	public Job(String largecd, String middlecd, String smallcd, String name) {
+	public Job(int id, String largecd, String middlecd, String smallcd, String name) {
+		this.id = id;
 		this.largecd = largecd;
 		this.middlecd = middlecd;
 		this.smallcd = smallcd;
 		this.name = name;
+	}
+
+	/**
+	 * 大分類コードを返す
+	 * @return id
+	 */
+	public int getId() {
+		return id;
 	}
 
 	/**
--- nexus/src/jp/or/adash/nexus/entity/Saiban.java
+++ nexus/src/jp/or/adash/nexus/entity/Saiban.java
@@ -11,18 +11,23 @@ public class Saiban {
 	 * jobseekersaiban 求職者ID採番
      * kyujinsaiban　求人NO採番
      * companysaiban　事業所番号採番
+     * staffsaiban 職業紹介者採番
 	 *
 	 */
 
 	private int jobseekersaiban;
 	private int kyujinsaiban;
 	private int companysaiban;
+	private int staffsaiban;
 
-	public Saiban(int jobseekersaiban, int kyujinsaiban, int companysaiban) {
+	public Saiban(int staffsaiban, int jobseekersaiban, int kyujinsaiban, int companysaiban) {
+		this.staffsaiban =staffsaiban;
 		this.jobseekersaiban = jobseekersaiban;
 		this.kyujinsaiban = kyujinsaiban;
 		this.companysaiban = companysaiban;
 	}
+
+
 
 	/**
 	 * 求職者ID採番を返す
@@ -48,5 +53,11 @@ public class Saiban {
 		return companysaiban;
 	}
 
-
+	/**
+	 *職業紹介者ID採番を返す
+	 * @return staffsaiban
+	 */
+	public int getStaffsaiban() {
+		return staffsaiban;
+	}
 }
