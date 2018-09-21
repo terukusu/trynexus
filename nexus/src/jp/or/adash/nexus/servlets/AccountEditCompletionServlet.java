a8bfa5d366e7e3f8a6b801461c616f0b96e56734
HayatoTomonari <tomonari1807adash@outlook.jp>
2018-09-21 11:41:51 +0900

    AccountEditCompletionServlet編集

--- nexus/src/jp/or/adash/nexus/servlets/AccountEditCompletionServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/AccountEditCompletionServlet.java
@@ -1,7 +1,6 @@
 package jp.or.adash.nexus.servlets;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,36 +13,46 @@ import jp.or.adash.nexus.entity.Staff;
 /**
  * Servlet implementation class AccountEditDisplayServlet
  */
-@WebServlet("/AccountEditDisplayServlet")
+@WebServlet("/AccountEditCompletionServlet")
 public class AccountEditCompletionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccountEditCompletionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AccountEditCompletionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String staffid = request.getParameter("id");
-		// 1.アカウントIDを元にアカウント情報を取得する
-		AccountListService service = new AccountListService();
-		List<Staff> list = service.getAccountList();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String kana = request.getParameter("kana");
+		String authority = request.getParameter("authority");
+		String password = request.getParameter("password");
+		//0アカウント情報のオブジェクトを作成
+		Staff staff = new Staff(id, name, kana, authority, password,
+				null, null, null, null, null);
+		// 1.アカウントIDを元にアカウント情報を更新する。
+		AccountEditService service = new AccountEditService();
+		service.updateStaff(staff);
 		// 2.アカウント情報をリクエストに格納する
-		request.setAttribute("accounts", list);
+		request.setAttribute("Staff", staff);
+		request.setAttribute("messages", service.getMessages());
 		// 3.JSPにフォワードする
-		request.getRequestDispatcher("./accountedit.jsp").forward(request, response);
-	}
+		request.getRequestDispatcher("./accounteditcompletion.jsp").forward(request, response);
 
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
