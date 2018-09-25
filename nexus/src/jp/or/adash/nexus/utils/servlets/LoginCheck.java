fc361e0ed478cc87bbc6e30343982eb21996d098
pgjavaAT <pgjavaAT@AW301.adash.local>
2018-09-25 10:23:24 +0900

    サーブレットフィルターを有効にする

--- nexus/src/jp/or/adash/nexus/utils/servlets/LoginCheck.java
+++ nexus/src/jp/or/adash/nexus/utils/servlets/LoginCheck.java
@@ -1,8 +1,21 @@
 package jp.or.adash.nexus.utils.servlets;
 
-/*
-@WebFilter("/*")
->>>>>>> refs/remotes/origin/#23
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import jp.or.adash.nexus.entity.Staff;
+
+@WebFilter("/web/*")
 public class LoginCheck implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -30,4 +43,3 @@ public class LoginCheck implements Filter {
 	public void destroy(){
 	}
 }
-*/
