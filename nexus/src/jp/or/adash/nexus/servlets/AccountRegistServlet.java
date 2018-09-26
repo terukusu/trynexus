8813ea8559966a6eb82cf18eaf3ba4a27d0be4ba
ishihamaguchi <ishida1807adash@outlook.jp>
2018-09-26 10:23:06 +0900

    アカウント登録編集

--- nexus/src/jp/or/adash/nexus/servlets/AccountRegistServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/AccountRegistServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import jp.or.adash.nexus.entity.Staff;
-import jp.or.adash.nexus.services.AccountEditService;
 import jp.or.adash.nexus.services.AccountService;
 
 
@@ -47,36 +46,23 @@ public class AccountRegistServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
-		//入力された情報を登録する
+		//0 入力された情報を登録する
 		String name = request.getParameter("name");
 		String kana = request.getParameter("kana");
 		String authority = request.getParameter("kengen");
 		String password = request.getParameter("password");
 
 
-		//0セッション　Objectはいろんな型を入れておける　ユーザーIdとユーザー名
+		//0 セッション　Objectはいろんな型を入れておける　ユーザーIdとユーザー名
 		HttpSession session = request.getSession(true);
 		Staff sessionStaff = (Staff)session.getAttribute("UserData");
 
-		//staffのオブジェクトにデータをセット ログインした人と、今から登録する人２人分のStaffオブジェクトが必要
+		//0 staffのオブジェクトにデータをセット ログインした人と、今から登録する人２人分のStaffオブジェクトが必要
 		Staff staff = new Staff(null, name, kana, authority, password,
 			null, sessionStaff.getId(), null, sessionStaff.getId(), "0");
 
-		//serviceのregistAccountにstaffを渡す
+		//0 serviceのregistAccountにstaffを渡す
 		AccountService accountservice = new AccountService();
-		//0		エラーチェック
-				AccountEditService service = new AccountEditService();
-				if (!service.errorsCheck(staff)) {
-
-					//0	アカウント情報をセット
-					request.setAttribute("staff", staff);
-					request.setAttribute("messages", service.getMessages());
-
-					//0	JSPにフォワード
-					 request.getRequestDispatcher("/accountregist.jsp").forward(request, response);
-					 //ここにjspを入力
-					return;
-				}
 		boolean result = accountservice.registAccount(staff);
 
 
