f9b395991e648fb2d367fdca967d2b7a9dafa5f7
pgjavaAT <pgjavaAT@AW301.adash.local>
2018-09-26 11:31:21 +0900

    #4の求職者情報、絞り込み検索機能実装

--- nexus/src/jp/or/adash/nexus/servlets/JobSeekerSearchServlet.java
+++ nexus/src/jp/or/adash/nexus/servlets/JobSeekerSearchServlet.java
@@ -8,44 +8,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import jp.or.adash.nexus.entity.Jobseeker_simple_entity;
-import jp.or.adash.nexus.entity.Staff;
+import jp.or.adash.nexus.entity.StaffName;
 import jp.or.adash.nexus.services.JobSeekerService;
 
 /**
- * Servlet implementation class JobSeekerShowNameServlet
+ * Servlet implementation class JobSeekerSearchServlet
  */
-@WebServlet("/web/jobseeker-showname")
-public class JobSeekerShowNameServlet extends HttpServlet {
+@WebServlet("/web/jobseeker-search")
+public class JobSeekerSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private String js_name;
+	private String js_id;
+	private String js_kana;
+	private String st_name;
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public JobSeekerShowNameServlet() {
+    public JobSeekerSearchServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		Staff staff = (Staff) session.getAttribute("UserData");
-
-
-		// 1.検索する求職者IDを取得する
-		this.js_name = request.getParameter("js_name");
+		// 1.検索する求職者ID、求職者かな名、担当者氏名を取得する
+		this.js_id = request.getParameter("js_id");
+		this.js_kana = request.getParameter("js_kana");
+		this.st_name = request.getParameter("st_name");
 		// 2.求職者情報一覧を取得する
 		JobSeekerService service = new JobSeekerService();
-		List<Jobseeker_simple_entity> list = service.getJobSeeker(js_name);
-		// 3.求職者情報を初期化
+		List<Jobseeker_simple_entity> list = service.getJobSeeker(js_id, js_kana, st_name);
+		// 3.担当紹介者氏名を取得する
+		List<StaffName> st_name = service.getTantoStaff();
+		// 4.求職者情報を初期化
 		request.removeAttribute("list");
-		// 4.求職者情報をリクエストに格納する
-		request.setAttribute("Staff", staff);
+		// 5.求職者情報、担当紹介者氏名をリクエストに格納する
 		request.setAttribute("list", list);
-		// 5.JSPにフォワードする
+		request.setAttribute("st_name", st_name);
+		// 6.JSPにフォワードする
 		request.getRequestDispatcher("/applicant_list.jsp").forward(request, response);
 
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
